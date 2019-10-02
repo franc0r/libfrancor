d179bad1f5a44fdb51f8ea8f8622a7195946a10b
Christian Merkl <knueppl@gmx.de>
2019-10-02 20:46:39 +0200

    POC new design for processing pipeline stage. Adaped existing processing stages.

--- francor_processing/include/francor_processing/data_processing_pipeline.h
+++ francor_processing/include/francor_processing/data_processing_pipeline.h
@@ -26,7 +26,7 @@ public:
   DataProcssingPipeline(void) = default;
   ~DataProcssingPipeline(void) = default;
 
-  bool addStage(std::unique_ptr<DataProcessingStage> stage)
+  bool addStage(std::unique_ptr<ProcessingStage> stage)
   {
     using francor::base::LogError;
     using francor::base::LogDebug;
@@ -42,126 +42,6 @@ public:
     LogDebug() << "DataProcessingPipeline: stage \"" << stage->name() << "\" successfully added to pipeline.";
     _stages.push_back(std::move(stage));
     return true;
-  }
-
-  bool connectInputWithOutput(const std::string& stageNameA,
-                              const std::string& inputName,
-                              const std::string& stageNameB,
-                              const std::string& outputName)
-  {
-    using francor::base::LogError;
-    using francor::base::LogDebug;
-    LogDebug() << "DataProcessingPipeline: try to connect input \"" << inputName << "\" of stage \"" << stageNameA
-               << "\" with output \"" << outputName << "\" of stage \"" << stageNameB << "\".";
-
-    // get required stages
-    const std::size_t stageIndexA = this->getStageIndexByName(stageNameA);
-    const std::size_t stageIndexB = this->getStageIndexByName(stageNameB);
-
-    if (stageIndexA >= _stages.size())
-    {
-      LogError() << "DataProcessingPipeline: stage \"" << stageNameA << "\" isn't contained in pipeline. Can't connect data ports";
-      return false;
-    }
-    if (stageIndexB >= _stages.size())
-    {
-      LogError() << "DataProcessingPipeline: stage \"" << stageNameB << "\" isn't contained in pipeline. Can't connect data ports";
-      return false;
-    }
-
-    // get input index
-    std::size_t inputIndex;
-
-    for (inputIndex = 0; inputIndex < _stages[stageIndexA]->numInputs(); ++inputIndex)
-      if (_stages[stageIndexA]->input(inputIndex).name() == outputName)
-        break;
-
-    if (inputIndex >= _stages[stageIndexA]->numInputs())
-    {
-      LogError() << "DataProcessingPipeline: input port \"" << inputName << "\" not found. Can't connect data ports.";
-      return false;
-    }
-
-    // get target output index
-    std::size_t outputIndex;
-    
-    for (outputIndex = 0; outputIndex < _stages[stageIndexB]->numOutputs(); ++outputIndex)
-      if (_stages[stageIndexB]->output(outputIndex).name() == outputName)
-        break;
-        
-    if (outputIndex >= _stages[stageIndexB]->numOutputs())
-    {
-      LogError() << "DataProcessingPipeline: output port \"" << outputName << "\" not found. Can't connect data ports.";
-      return false;
-    }
-
-    LogDebug() << "DataProcessingPipeline: data ports successfully connected.";
-    return _stages[stageIndexA]->input(inputIndex).connect(_stages[stageIndexB]->output(outputIndex));
-  }
-
-  bool connectDataSourcePort(data::SourcePort& port, const std::string& stageName, const std::string& inputName)
-  {
-    using francor::base::LogError;
-    using francor::base::LogDebug;
-    LogDebug() << "DataProcessingPipeline: try to connect input \"" << inputName << "\" of stage \"" << stageName
-               << "\" with data source \"" << port.name() << "\".";
-
-    // get required stage
-    const std::size_t stageIndex = this->getStageIndexByName(stageName);
-
-    if (stageIndex >= _stages.size())
-    {
-      LogError() << "DataProcessingPipeline: stage \"" << stageName << " isn't contained in pipeline. Can't connect data ports";
-      return false;
-    }
-
-    // get input port index
-    std::size_t inputIndex;
-
-    for (inputIndex = 0; inputIndex < _stages[stageIndex]->numInputs(); ++inputIndex)
-      if (_stages[stageIndex]->input(inputIndex).name() == inputName)
-        break;
-
-    if (inputIndex >= _stages[stageIndex]->numInputs())
-    {
-      LogError() << "DataProcessingPipeline: input port \"" << inputName << "\" not found. Can't connect data ports.";
-      return false;
-    }
-
-    LogDebug() << "DataProcessingPipeline: data ports successfully connected.";
-    return _stages[stageIndex]->input(inputIndex).connect(port);
-  }
-
-  bool connectDataDestinationPort(data::DestinationPort& port, const std::string& stageName, const std::string& outputName)
-  {
-    using francor::base::LogError;
-    using francor::base::LogDebug;
-    LogDebug() << "DataProcessingPipeline: try to connect output \"" << outputName << "\" of stage \"" << stageName
-               << "\" with data destination port \"" << port.name() << "\".";    
-    // get required stage
-    const std::size_t stageIndex = this->getStageIndexByName(stageName);
-
-    if (stageIndex >= _stages.size())
-    {
-      LogError() << "DataProcessingPipeline: stage \"" << stageName << " isn't contained in pipeline. Can't connect data ports";
-      return false;
-    }
-
-    // get input port index
-    std::size_t outputIndex;
-
-    for (outputIndex = 0; outputIndex < _stages[stageIndex]->numOutputs(); ++outputIndex)
-      if (_stages[stageIndex]->output(outputIndex).name() == outputName)
-        break;
-
-    if (outputIndex >= _stages[stageIndex]->numOutputs())
-    {
-      LogError() << "DataProcessingPipeline: output port \"" << outputName << "\" not found. Can't connect data ports.";
-      return false;
-    }
-
-    LogDebug() << "DataProcessingPipeline: data ports successfully connected.";
-    return _stages[stageIndex]->output(outputIndex).connect(port);
   }
 
   bool initialize(void)
@@ -198,7 +78,7 @@ private:
   {
     return std::find_if(_stages.begin(),
                         _stages.end(),
-                        [&] (const std::unique_ptr<DataProcessingStage>& stage) { return stage->name() == stageName; } )
+                        [&] (const std::unique_ptr<ProcessingStage>& stage) { return stage->name() == stageName; } )
            !=
            _stages.end();
   }
@@ -211,7 +91,7 @@ private:
     return _stages.size();
   }
 
-  std::vector<std::unique_ptr<DataProcessingStage>> _stages;
+  std::vector<std::unique_ptr<ProcessingStage>> _stages;
 };
 
 } // end namespace processing
--- francor_processing/include/francor_processing/data_processing_pipeline_stage.h
+++ francor_processing/include/francor_processing/data_processing_pipeline_stage.h
@@ -19,6 +19,7 @@
 #include "francor_processing/data_processing_pipeline_port_block.h"
 
 #include <string>
+#include <stdexcept>
 
 namespace francor
 {
@@ -26,37 +27,51 @@ namespace francor
 namespace processing
 {
 
-class DiagnosticData { };
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Data Processing Stage
-template <std::size_t NumOfInputs, std::size_t NumOfOutputs>
 class ProcessingStage
 {
 protected:
   ProcessingStage() = delete;
   ProcessingStage(const std::string& stageName) : _name(stageName) { }
-  ProcessingStage(const ProcessingStage&) = delete;
-  ProcessingStage(ProcessingStage&&) = delete;
-
-  ProcessingStage& operator=(const ProcessingStage&) = delete;
-  ProcessingStage& operator=(ProcessingStage&&) = delete;
 
 public:
-  virtual ~ProcessingStage(void) = default;
+  virtual ~ProcessingStage() = default;
 
   bool process()
   {
-    this->doProcess();
+    bool ret = this->doProcess();
+    // TODO: add diagnostic
+
+    return ret;
   }
   virtual bool initialize() = 0;
+
+  inline const std::string& name() const noexcept { return _name; }
+
+protected:
+  virtual bool doProcess() = 0;
+  
+private:
+  const std::string _name;
+};
+
+template <std::size_t NumOfInputs, std::size_t NumOfOutputs>
+class DataInputOutput
+{
+public:
+  DataInputOutput() { this->initialize(); };
+  DataInputOutput(const DataInputOutput&) = delete;
+  virtual ~DataInputOutput() = default;
+
+  DataInputOutput& operator=(const DataInputOutput&) = delete;
+
   const data::InputPort& input(const std::string& portName) const
   {
     for (auto& port : _input_ports)
       if (port.name() == portName)
         return port;
 
-    throw "Processing Stage: input port name is unkown.";
+    base::LogError() << "Processing Stage: input port name is unkown.";
+    throw std::invalid_argument("Processing Stage: input port name is unkown.");
   }
   const data::OutputPort& output(const std::string& portName) const
   {
@@ -64,25 +79,50 @@ public:
       if (port.name() == portName)
         return port;
 
-    throw "Processing Stage: output port name is unkown.";
+    base::LogError() << "Processing Stage: output port name is unkown.";
+    throw std::invalid_argument("Processing Stage: output port name is unkown.");
   }
-
-  inline const std::string& name() const noexcept { return _name; }
 
 protected:
   inline std::array<data::InputPort, NumOfInputs>& getInputs() { return _input_ports; }
   inline std::array<data::OutputPort, NumOfOutputs>& getOutputs() { return _output_ports; }
 
-  virtual bool doProcess() = 0;
-  virtual DiagnosticData getDiagnostic() const = 0;
-  
+  virtual void initializePorts() = 0;
+
+  template <typename DataType>
+  inline void initializeInputPort(const std::size_t index, const std::string& name)
+  {
+    _input_ports[index] = data::InputPort::create<DataType>(name);
+  }
+  template <typename DataType>
+  void initializeOutputPort(const std::size_t index, const std::string& name, DataType const* const data)
+  {
+    _output_ports[index] = data::OutputPort::create(name, data);
+  }
+
 private:
-  const std::string _name;
+  void initialize()
+  {
+    this->initializePorts();
+
+    for (const auto& input : _input_ports)
+      ; // TODO: check if input is initialized
+
+    for (const auto& output : _output_ports)
+      ; // TODO: check if output is initialized
+  }
+
   std::array<data::InputPort, NumOfInputs> _input_ports;
   std::array<data::OutputPort, NumOfOutputs> _output_ports;
 };
 
 
+template <std::size_t NumOfInputs, std::size_t NumOfOutputs>
+class ProcessingStageParent : public ProcessingStage, public DataInputOutput<NumOfInputs, NumOfOutputs>
+{
+public:
+  ProcessingStageParent(const std::string& name) : ProcessingStage(name), DataInputOutput<NumOfInputs, NumOfOutputs>() { }
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Data Processing Stage Detect Lines
@@ -92,36 +132,36 @@ using francor::base::LineVector;
 /**
  * \brief This class searches lines in a 2d point set using a line ransac.
  */
-class DetectLines : public ProcessingStageIO<2, 1>
+class DetectLines : public ProcessingStageParent<2, 1>
 {
 public:
   DetectLines(const unsigned int maxIterations = 100, const std::size_t minNumPoints = 2, const double epsilon = 0.3)
-    : ProcessingStageIO<2, 1>("detect lines")
+    : ProcessingStageParent<2, 1>("detect lines")
   {
     _detector.setMaxIterations(maxIterations);
     _detector.setMinNumPoints(minNumPoints);
     _detector.setEpsilon(epsilon);
   }
-  virtual ~DetectLines(void) = default;
+  ~DetectLines(void) = default;
 
-  virtual bool process(void) override final
+  bool doProcess(void) final
   {
     using francor::base::LogDebug;
 
     LogDebug() << this->name() << ": start data procssing.";
     _lines.clear();
 
-    if (this->inputs().port(0).numOfConnections() > 0)
+    if (this->getInputs()[0].numOfConnections() > 0)
     {
       LogDebug() << this->name() << ": process with a single set of 2d points.";
-      _lines = _detector(this->inputs().port(0).data<VectorVector2d>());
+      _lines = _detector(this->getInputs()[0].data<VectorVector2d>());
     }
-    if (this->inputs().port(1).numOfConnections() > 0)
+    if (this->getInputs()[1].numOfConnections() > 0)
     {
-      LogDebug() << this->name() << ": process with " << this->inputs().port(1).data<std::vector<VectorVector2d>>().size()
+      LogDebug() << this->name() << ": process with " << this->getInputs()[1].data<std::vector<VectorVector2d>>().size()
                  << " sets of 2d points.";
 
-      for (const auto& cluster : this->inputs().port(1).data<std::vector<VectorVector2d>>())
+      for (const auto& cluster : this->getInputs()[1].data<std::vector<VectorVector2d>>())
       {
         base::LineVector lines(_detector(cluster));
         _lines.insert(_lines.end(), lines.begin(), lines.end());
@@ -133,22 +173,18 @@ public:
     LogDebug() << this->name() << ": finished data processing.";
     return true;
   }
-
-private:
-  virtual bool configurePorts(InputPortBlock<num_inputs>& inputs, OutputPortBlock<num_outputs>& outputs) override final
-  {
-    bool ret = true;
-
-    ret &= inputs.configurePort<VectorVector2d>(0, "2d points");
-    ret &= inputs.configurePort<std::vector<VectorVector2d>>(1, "clustered 2d points");
-
-    ret &= outputs.configurePort<LineVector>(0, "2d lines", &_lines);
-
-    return ret;
-  }
-  virtual bool configureProcessing(void) override final
+  bool initialize() final
   {
     return true;
+  }
+
+private:
+  void initializePorts() final
+  {
+    this->initializeInputPort<VectorVector2d>(0, "2d points");
+    this->initializeInputPort<std::vector<VectorVector2d>>(1, "clustered 2d points");
+
+    this->initializeOutputPort(0, "2d lines", &_lines);
   }
 
   LineVector _lines;
@@ -158,36 +194,36 @@ private:
 /**
  * \brief This class searches lines in a 2d point set using a line ransac.
  */
-class DetectLineSegments : public ProcessingStageIO<2, 1>
+class DetectLineSegments : public ProcessingStageParent<2, 1>
 {
 public:
   DetectLineSegments(const unsigned int maxIterations = 100, const std::size_t minNumPoints = 2, const double epsilon = 0.3)
-    : ProcessingStageIO<2, 1>("detect line segments")
+    : ProcessingStageParent<2, 1>("detect line segments")
   {
     _detector.setMaxIterations(maxIterations);
     _detector.setMinNumPoints(minNumPoints);
     _detector.setEpsilon(epsilon);
   }
-  virtual ~DetectLineSegments(void) = default;
+  ~DetectLineSegments(void) = default;
 
-  virtual bool process(void) override final
+  bool doProcess(void) final
   {
     using francor::base::LogDebug;
 
     LogDebug() << this->name() << ": start data procssing.";
     _lines.clear();
 
-    if (this->inputs().port(0).numOfConnections() > 0)
+    if (this->getInputs()[0].numOfConnections() > 0)
     {
       LogDebug() << this->name() << ": process with a single set of 2d points.";
-      _lines = _detector(this->inputs().port(0).data<VectorVector2d>());
+      _lines = _detector(this->getInputs()[0].data<VectorVector2d>());
     }
-    if (this->inputs().port(1).numOfConnections() > 0)
+    if (this->getInputs()[1].numOfConnections() > 0)
     {
-      LogDebug() << this->name() << ": process with " << this->inputs().port(1).data<std::vector<VectorVector2d>>().size()
+      LogDebug() << this->name() << ": process with " << this->getInputs()[1].data<std::vector<VectorVector2d>>().size()
                  << " sets of 2d points.";
 
-      for (const auto& cluster : this->inputs().port(1).data<std::vector<VectorVector2d>>())
+      for (const auto& cluster : this->getInputs()[1].data<std::vector<VectorVector2d>>())
       {
         base::LineSegmentVector lines(_detector(cluster));
         _lines.insert(_lines.end(), lines.begin(), lines.end());
@@ -199,22 +235,18 @@ public:
     LogDebug() << this->name() << ": finished data processing.";
     return true;
   }
-
-private:
-  virtual bool configurePorts(InputPortBlock<num_inputs>& inputs, OutputPortBlock<num_outputs>& outputs) override final
-  {
-    bool ret = true;
-
-    ret &= inputs.configurePort<VectorVector2d>(0, "2d points");
-    ret &= inputs.configurePort<std::vector<VectorVector2d>>(1, "clustered 2d points");
-
-    ret &= outputs.configurePort<base::LineSegmentVector>(0, "2d line segments", &_lines);
-
-    return ret;
-  }
-  virtual bool configureProcessing(void) override final
+  bool initialize(void) final
   {
     return true;
+  }
+
+private:
+  void initializePorts() final
+  {
+    this->initializeInputPort<VectorVector2d>(0, "2d points");
+    this->initializeInputPort<std::vector<VectorVector2d>>(1, "clustered 2d points");
+
+    this->initializeOutputPort(0, "2d line segments", &_lines);
   }
 
   base::LineSegmentVector _lines;
@@ -223,17 +255,17 @@ private:
 
 using francor::vision::Image;
   
-class ExportClusteredPointsFromBitMask : public ProcessingStageIO<1, 1>
+class ExportClusteredPointsFromBitMask : public ProcessingStageParent<1, 1>
 {
 public:
   ExportClusteredPointsFromBitMask(void)
-    : ProcessingStageIO<1, 1>("export clustered points from bit mask") {  }
-  virtual ~ExportClusteredPointsFromBitMask(void) = default;
+    : ProcessingStageParent<1, 1>("export clustered points from bit mask") {  }
+  ~ExportClusteredPointsFromBitMask(void) = default;
 
-  virtual bool process(void) override final
+  bool doProcess(void) final
   {
     // input data type safety check
-    if (this->inputs().port(0).data<Image>().colourSpace() != vision::ColourSpace::BIT_MASK)
+    if (this->getInputs()[0].data<Image>().colourSpace() != vision::ColourSpace::BIT_MASK)
     {
       //TODO: print error
       return false;
@@ -242,7 +274,7 @@ public:
     // find contours using opencv function
     std::vector<std::vector<cv::Point2i>> foundClusters;
 
-    cv::findContours(this->inputs().port(0).data<Image>().cvMat(), foundClusters, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    cv::findContours(this->getInputs()[0].data<Image>().cvMat(), foundClusters, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
 
     // copy data to francor data types
     _clustered_points.resize(foundClusters.size());
@@ -260,23 +292,17 @@ public:
 
     return true;
   }
-
-private:
-  virtual bool configurePorts(InputPortBlock<num_inputs>& inputs, OutputPortBlock<num_outputs>& outputs) override final
-  {
-    bool ret = true;
-
-    ret &= inputs.configurePort<Image>(0, "bit mask");
-    ret &= outputs.configurePort<std::vector<VectorVector2d>>(0, "clustered 2d points", &_clustered_points);
-
-    return ret;
-  }
-
-  virtual bool configureProcessing(void) override final
+  bool initialize(void) final
   {
     return true;
   }
 
+private:
+  void initializePorts() final
+  {
+    this->initializeInputPort<Image>(0, "bit mask");
+    this->initializeOutputPort<std::vector<VectorVector2d>>(0, "clustered 2d points", &_clustered_points);
+  }
 
   std::vector<VectorVector2d> _clustered_points;
 };                                        
@@ -287,45 +313,41 @@ using francor::vision::ColourSpace;
 using francor::vision::ImageMaskFilterPipeline;
 using francor::vision::ImageMaskFilterColourRange;
 
-class ColouredImageToBitMask : public ProcessingStageIO<1, 1>
+class ColouredImageToBitMask : public ProcessingStageParent<1, 1>
 {
 public:
   ColouredImageToBitMask(void)
-    : ProcessingStageIO<1, 1>("coloured image to bit mask") {  }
-  virtual ~ColouredImageToBitMask(void) = default;
+    : ProcessingStageParent<1, 1>("coloured image to bit mask") {  }
+  ~ColouredImageToBitMask(void) = default;
 
-  virtual bool process(void) override final
+  bool doProcess(void) final
   {
     // check colour space of input image
-    if (this->inputs().port(0).data<Image>().colourSpace() != ColourSpace::BGR
+    if (this->getInputs()[0].data<Image>().colourSpace() != ColourSpace::BGR
         &&
-        this->inputs().port(0).data<Image>().colourSpace() != ColourSpace::RGB
+        this->getInputs()[0].data<Image>().colourSpace() != ColourSpace::RGB
         &&
-        this->inputs().port(0).data<Image>().colourSpace() != ColourSpace::HSV)
+        this->getInputs()[0].data<Image>().colourSpace() != ColourSpace::HSV)
     {
       //TODO: print error
       return false;
     }
 
-    return _image_pipeline(this->inputs().port(0).data<Image>(), _bit_mask);
+    return _image_pipeline(this->getInputs()[0].data<Image>(), _bit_mask);
   }
 
-private:
-  virtual bool configurePorts(InputPortBlock<num_inputs>& inputs, OutputPortBlock<num_outputs>& outputs) override final
-  {
-    bool ret = true;
-
-    ret &= inputs.configurePort<Image>(0, "coloured image");
-    ret &= outputs.configurePort<Image>(0, "bit mask", &_bit_mask);
-
-    return ret;
-  }
-
-  virtual bool configureProcessing(void) override final
+  bool initialize(void) final
   {
     auto rangeFilter = std::make_unique<ImageMaskFilterColourRange>(100, 120, 70, 255, 30, 255);
     
     return _image_pipeline.addFilter("colour range", std::move(rangeFilter));
+  }
+  
+private:
+  void initializePorts() final
+  {
+    this->initializeInputPort<Image>(0, "coloured image");
+    this->initializeOutputPort<Image>(0, "bit mask", &_bit_mask);
   }
 
   Image _bit_mask;
