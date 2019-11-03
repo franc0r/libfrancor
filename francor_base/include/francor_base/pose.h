6a460d853627b41cadeba6b44a4d8caa4db62e40
Christian Merkl <knueppl@gmx.de>
2019-11-03 16:40:59 +0100

    Added shift operator for pose2d.

--- francor_base/include/francor_base/pose.h
+++ francor_base/include/francor_base/pose.h
@@ -9,6 +9,8 @@
 #include "francor_base/point.h"
 #include "francor_base/angle.h"
 
+#include <ostream>
+
 namespace francor {
 
 namespace base {
@@ -16,6 +18,7 @@ namespace base {
 class Pose2d
 {
 public:
+  constexpr Pose2d() : _position(0.0, 0.0), _orientation(0.0) { }
   constexpr Pose2d(const Point2d& pos, const Angle angle) : _position(pos), _orientation(angle) { }
 
   inline constexpr const Point2d& position() const noexcept { return _position; }
@@ -31,3 +34,14 @@ private:
 } // end namespace base
 
 } // end namespace francor
+
+
+namespace std {
+
+inline ostream& operator<<(ostream& os, const francor::base::Pose2d& pose)
+{
+  os << "pose: [" << pose.orientation() << ", " << pose.position() << "]";
+  return os;
+}
+
+} // end namespace std
