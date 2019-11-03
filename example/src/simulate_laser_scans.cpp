8df8f2985e08361f9fc56c804f93d9a2be3984d5
Christian Merkl <knueppl@gmx.de>
2019-11-03 20:51:28 +0100

    Added opencv highgui modul as dependcy. Improved simulate laser scan example with gui debug output.

--- example/src/simulate_laser_scans.cpp
+++ example/src/simulate_laser_scans.cpp
@@ -14,16 +14,18 @@ using francor::mapping::PipeSimulateLaserScan;
 using francor::mapping::OccupancyGrid;
 using francor::mapping::EgoObject;
 using francor::vision::Image;
+using francor::vision::ColourSpace;
 using francor::base::Pose2d;
 using francor::base::Point2dVector;
 using francor::base::Transform2d;
 using francor::base::LogError;
 using francor::base::LogInfo;
 using francor::base::Vector2d;
+using francor::base::Angle;
 
 OccupancyGrid _grid;
 EgoObject _ego;
-const Pose2d _sensor_pose({ 0.0, 0.0 }, 0.0);
+const Pose2d _sensor_pose({ 0.0, 0.0 }, Angle::createFromDegree(0.0));
 
 PipeSimulateLaserScan pipeline;
 
@@ -33,9 +35,26 @@ void drawPoseOnImage(const Pose2d& pose, Image& image)
   const auto index_y = _grid.getIndexY(pose.position().y());
   constexpr std::size_t axis_length = 20;
 
-  for (std::size_t i = 0; i < axis_length; ++i) {
-    image(index_y, index_x + i).gray() = 0;
-    image(index_y + i, index_x).gray() = 0;
+  for (std::size_t i = 0; i < axis_length; ++i)
+  {
+    image(index_y, index_x + i).r() = 255;
+    image(index_y, index_x + i).g() =   0;
+    image(index_y, index_x + i).b() =   0;
+
+    image(index_y + i, index_x).r() =   0;
+    image(index_y + i, index_x).g() = 255;
+    image(index_y + i, index_x).b() =   0;
+  }
+}
+
+void drawPointsOnImage(const Point2dVector& points, Image& image)
+{
+  for (const auto& point : points)
+  {
+    const auto index_x = _grid.getIndexX(point.x());
+    const auto index_y = _grid.getIndexY(point.y());
+
+    cv::circle(image.cvMat(), { index_x, index_y }, 7, cv::Scalar(0, 0, 255), 3);
   }
 }
 
@@ -57,6 +76,7 @@ bool initialize(const std::string& file_name)
   // std::cout << _grid << std::endl;
   Image out;
   francor::mapping::algorithm::occupancy::convertGridToImage(_grid, out);
+  out.transformTo(ColourSpace::BGR);
   drawPoseOnImage(_ego.pose(), out);
   francor::vision::saveImageToFile("/tmp/occupancy_grid.png", out);
 
@@ -82,9 +102,13 @@ bool processStep(const Vector2d& delta_position)
 
   Image out;
   francor::mapping::algorithm::occupancy::convertGridToImage(_grid, out);
+  out.transformTo(ColourSpace::BGR);
   drawPoseOnImage(_ego.pose(), out);
+  drawPointsOnImage(pipeline.output(PipeSimulateLaserScan::OUT_POINTS).data<Point2dVector>(), out);
+  cv::imshow("occupancy grid", out.cvMat());
+  cv::waitKey(10);
   francor::vision::saveImageToFile("/tmp/occupancy_grid.png", out);
-  std::cout << "result:" << std::endl << pipeline.output(PipeSimulateLaserScan::OUT_POINTS).data<Point2dVector>() << std::endl;
+  // std::cout << "result:" << std::endl << pipeline.output(PipeSimulateLaserScan::OUT_POINTS).data<Point2dVector>() << std::endl;
   return true;
 }
 
@@ -103,7 +127,7 @@ int main(int argc, char** argv)
     return 2;
   }
 
-  for (std::size_t step = 0; step < 10; ++step)
+  for (std::size_t step = 0; step < 500; ++step)
   {
     const Vector2d step_position(0.0, 0.05);
 
@@ -112,7 +136,7 @@ int main(int argc, char** argv)
       return 3;
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
   return 0;
