e198c73bad618d7d29626cd471d99b51df12d1ac
Christian Merkl <c.merkl@abidat.de>
2019-12-28 22:50:46 +0100

    Added binomial distribution template class.

--- francor_base/include/francor_base/algorithm/math.h
+++ francor_base/include/francor_base/algorithm/math.h
@@ -15,20 +15,46 @@ namespace algorithm {
 
 namespace math {
 
-template <typename Type, Type... K>
-inline Type fak(std::integer_sequence<Type, K...>)
+template <unsigned int EXP, typename Type = float>
+struct pow
 {
-  return ((K + 1) * ...);
+  pow() = delete;
+  constexpr pow(const Type base) : value(calculate(base, std::make_integer_sequence<std::size_t, EXP>{})) { }
+  const Type value;
+  inline constexpr operator Type() const { return value; }
+
+private:
+  template <std::size_t... I>
+  static constexpr Type calculate(const Type base, std::integer_sequence<std::size_t, I...>)
+  {
+    if constexpr (EXP == 0) {
+      return static_cast<Type>(1);
+    }
+    else {
+      return ((I * 0 + base) * ...);
+    }
+  }
+};
+
+template <typename Type, unsigned int... K>
+inline constexpr Type fak(std::integer_sequence<unsigned int, K...>)
+{
+  if constexpr (sizeof...(K) == 0) {
+    return static_cast<Type>(1);
+  }
+  else {
+    return (static_cast<Type>(K + 1) * ...);
+  }
 }
 
-template <std::size_t N, typename Type, typename Indices = std::make_integer_sequence<Type, N>>
-inline Type fak()
+template <std::size_t N, typename Type, typename Indices = std::make_integer_sequence<unsigned int, N>>
+inline constexpr Type fak()
 {
   return fak<Type>(Indices{});
 }
 
 template <typename Type>
-inline Type fak(const Type value)
+inline constexpr Type fak(const Type value)
 {
   Type result = 1;
 
@@ -38,6 +64,36 @@ inline Type fak(const Type value)
 
   return result;
 }
+
+template <unsigned int N, unsigned int K, typename Type = float>
+struct binomial_coefficient
+{
+  static_assert(K <= N, "Support parameter K must be smaller equal N.");
+
+  static constexpr Type value = fak<N, Type>() / (fak<K, Type>() * fak<N - K, Type>());
+};
+
+template <unsigned int N, unsigned int P, typename Type = float>
+struct binomial_distribution
+{
+  static_assert(P <= N, "Template parameter P must be in range 0 .. N.");
+
+  using type = Type;
+
+  static constexpr Type p = static_cast<Type>(P) / static_cast<Type>(N);
+  static constexpr Type q = static_cast<Type>(1) - p;
+  static constexpr unsigned int n = N;
+  static constexpr Type variance = static_cast<Type>(n) * p * q;
+  static constexpr Type mean = n * p;
+  
+  template <unsigned int K>
+  struct pmf
+  {
+    static_assert(K <= N, "Template parameter K must be smaller equal N.");
+
+    static constexpr Type value = binomial_coefficient<N, K, Type>::value * pow<K, Type>(p) * pow<N - K, Type>(q);
+  };
+};
 
 } // end namespace point
 
--- francor_base/test/src/unit_test_math.cpp
+++ francor_base/test/src/unit_test_math.cpp
@@ -18,6 +18,36 @@ TEST(Fak, Functionality)
 
 }
 
+TEST(Pow, Functionality)
+{
+  EXPECT_EQ((francor::base::algorithm::math::pow<8, int>(2)), 256);
+}
+
+TEST(BinomialDistribution, Functionality)
+{
+  using distribution = francor::base::algorithm::math::binomial_distribution<10, 5, float>;
+
+  EXPECT_EQ(distribution::n, 10);
+  EXPECT_NEAR(distribution::p, 0.5f, 1e-6);
+  EXPECT_NEAR(distribution::q, 0.5f, 1e-6);
+  EXPECT_NEAR(distribution::variance, 2.5, 1e-6);
+  EXPECT_NEAR(distribution::mean, 5.0f, 1e-6);
+
+  EXPECT_NEAR(distribution::pmf<0>::value, 0.000976, 1e-6);
+  EXPECT_NEAR(distribution::pmf<1>::value, 0.009765, 1e-6);
+  EXPECT_NEAR(distribution::pmf<2>::value, 0.043945, 1e-6);
+  EXPECT_NEAR(distribution::pmf<3>::value, 0.117188, 1e-6);
+  EXPECT_NEAR(distribution::pmf<4>::value, 0.205078, 1e-6);
+
+  EXPECT_NEAR(distribution::pmf<5>::value, 0.246094, 1e-6);
+
+  EXPECT_NEAR(distribution::pmf<4>::value, distribution::pmf< 6>::value, 1e-6);
+  EXPECT_NEAR(distribution::pmf<3>::value, distribution::pmf< 7>::value, 1e-6);
+  EXPECT_NEAR(distribution::pmf<2>::value, distribution::pmf< 8>::value, 1e-6);
+  EXPECT_NEAR(distribution::pmf<1>::value, distribution::pmf< 9>::value, 1e-6);
+  EXPECT_NEAR(distribution::pmf<0>::value, distribution::pmf<10>::value, 1e-6);
+}
+
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
