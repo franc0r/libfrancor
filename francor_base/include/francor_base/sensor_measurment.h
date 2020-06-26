dd3cbf29895299bf6c2b38aa8d9c7261abf6803b
Christian Merkl <knueppl@gmx.de>
2020-06-26 18:59:51 +0200

    Added comments to the SensorMeasurement class definition.

--- francor_base/include/francor_base/sensor_measurment.h
+++ francor_base/include/francor_base/sensor_measurment.h
@@ -15,20 +15,31 @@ namespace base {
 class SensorMeasurment
 {
 public:
+  // delete default constructor
   SensorMeasurment() = delete;
+  // virtual destructor
   virtual ~SensorMeasurment() = default;
 
+  // to provide default values
+  // \todo think about the correct location for SensorName and SensorType.
   struct SensorName {
     static constexpr const char* NONE = "none";
   };
 
   struct SensorType {
-    static constexpr const char* NONE              = "no_type";
-    static constexpr const char* LIDAR_2D          = "lidar_2d";
-    static constexpr const char* LIDAR_2D_ROTATION = "lidar_2d_rotation";
+    static constexpr const char* NONE              = "no_type";           //> in case to type is available
+    static constexpr const char* LIDAR_2D          = "lidar_2d";          //> represents all types of 2d lidars
+    static constexpr const char* LIDAR_2D_ROTATION = "lidar_2d_rotation"; //> represents lidars with rotating measurement unit or mirror
   };
 
 protected:
+  /**
+   * \brief Constructs with all necessary attributes for a sensor measurment.
+   * 
+   * \param sensor_name Name of the sensor that measured this data.
+   * \param sensor_type The type of the sensor that measured this data.
+   * \param sensor_pose The pose of the sensor that measured this data. (in ego/vehicle) frame)
+   */
   SensorMeasurment(const std::string& sensor_name, const char* sensor_type, const Pose2d& sensor_pose)
     : _sensor_name(sensor_name),
       _sensor_type(sensor_type),
@@ -36,8 +47,23 @@ protected:
   { }
 
 public:
+  /**
+   * \brief Returns the pose of the sensor that measured this data.
+   * 
+   * \return 2d sensor pose.
+   */
   inline const Pose2d& sensorPose() const noexcept { return _sensor_pose; } 
+  /**
+   * \brief Returns the name of the sensor that measured this data.
+   * 
+   * \return Sensor name.
+   */
   inline const std::string& sensorName() const noexcept { return _sensor_name; }
+  /**
+   * \brief Returns the type of the sensor that measured this data.
+   * 
+   * \return Sensor type.
+   */
   inline const char* sensorType() const noexcept { return _sensor_type; } 
 
 private:
